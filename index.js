41bef60def0fd7d57805de6403070fc76b4afab0
NikhilPaleti <paletinikhil@gmail.com>
2024-09-24 01:00:43 +0530

    Animations added
    Somewhat.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.getElementById("ruleList").style.display="none"
 document.getElementById("resultSheet").style.display="none"
 document.getElementById("winSheet").style.display="none"
 
-
+//This is to load the initial score on the front-page
 if (localStorage.getItem('SPS_User_Score') !== null && localStorage.getItem('SPS_Opp_Score') !== null) {
     document.getElementById("mainYourScoreVAR").innerHTML = localStorage.getItem('SPS_User_Score');
     document.getElementById("mainOppScoreVAR").innerHTML = localStorage.getItem('SPS_Opp_Score');
@@ -14,7 +14,7 @@ else {
     document.getElementById("mainOppScoreVAR").innerHTML = localStorage.getItem('SPS_Opp_Score');
 }
 
-
+//Managing the rules button/list
 const ruleButton = document.getElementById("rules")
 ruleButton.addEventListener("click", () => {
     if(document.getElementById("ruleList").style.display=="none"){
@@ -24,7 +24,30 @@ ruleButton.addEventListener("click", () => {
         document.getElementById("ruleList").style.display = "none";
     }
 });
+//Managing the "next" button if you win
+const nextButton = document.getElementById("next")
+nextButton.addEventListener("click", () => {
+    document.getElementById("resultSheet").style.display="none";
+    document.getElementById("mainSheet").style.display="none";
+    document.getElementById("winSheet").style.display="flex";
+    nextButton.style.display="none";
+});
+//Managing the Play Again button
+const PAButton = document.getElementById("playAgain")
+PAButton.addEventListener("click", () => {
+    location.reload();
 
+});
+const PAButton2 = document.getElementById("playAgain2")
+PAButton2.addEventListener("click", () => {
+    location.reload();
+
+});
+
+
+const loser = document.getElementById("resLoss");
+const winner = document.getElementById("resWin");
+const tie = document.getElementById("resTie");
 
 
 const stoneButton = document.getElementById("stone");
@@ -39,6 +62,7 @@ stoneButton.addEventListener("click", () => {
         document.getElementById("resultSheet").style.display="block";
         document.getElementById("yourResStone").style.display="block";
         document.getElementById("compResStone").style.display="block";
+        tie.style.display="block";
         
         document.getElementById("resYourScoreVAR").innerHTML = localStorage.getItem('SPS_User_Score');
         document.getElementById("resOppScoreVAR").innerHTML = localStorage.getItem('SPS_Opp_Score');
@@ -47,7 +71,10 @@ stoneButton.addEventListener("click", () => {
         document.getElementById("mainSheet").style.display="none";
         document.getElementById("resultSheet").style.display="block";
         document.getElementById("yourResStone").style.display="block";
+        document.getElementById("yourResStone").style.animation = "lose-breathe 4s ease-in-out infinite"
         document.getElementById("compResPaper").style.display="block";
+        document.getElementById("compResPaper").style.animation = "win-breathe 4s ease-in-out infinite"
+        loser.style.display="block";
 
         localStorage.setItem('SPS_Opp_Score', parseInt(localStorage.getItem('SPS_Opp_Score'))+1);
         document.getElementById("resYourScoreVAR").innerHTML = localStorage.getItem('SPS_User_Score');
@@ -57,7 +84,12 @@ stoneButton.addEventListener("click", () => {
         document.getElementById("mainSheet").style.display="none";
         document.getElementById("resultSheet").style.display="block";
         document.getElementById("yourResStone").style.display="block";
+        document.getElementById("yourResStone").style.animation = "win-breathe 4s ease-in-out infinite"
         document.getElementById("compResScissor").style.display="block";
+        document.getElementById("compResScissor").style.animation = "lose-breathe 4s ease-in-out infinite"
+        nextButton.style.display="block";
+        winner.style.display="block";
+
 
         localStorage.setItem('SPS_User_Score', parseInt(localStorage.getItem('SPS_User_Score'))+1);
         document.getElementById("resYourScoreVAR").innerHTML = localStorage.getItem('SPS_User_Score');
@@ -73,7 +105,11 @@ paperButton.addEventListener("click", () => {
         document.getElementById("mainSheet").style.display="none";
         document.getElementById("resultSheet").style.display="block";
         document.getElementById("yourResPaper").style.display="block";
+        document.getElementById("yourResPaper").style.animation = "win-breathe 4s ease-in-out infinite"
         document.getElementById("compResStone").style.display="block";
+        document.getElementById("compResStone").style.animation = "lose-breathe 4s ease-in-out infinite"
+        nextButton.style.display="block";
+        winner.style.display="block";
         
         localStorage.setItem('SPS_User_Score', parseInt(localStorage.getItem('SPS_User_Score'))+1);
         document.getElementById("resYourScoreVAR").innerHTML = localStorage.getItem('SPS_User_Score');
@@ -84,6 +120,7 @@ paperButton.addEventListener("click", () => {
         document.getElementById("resultSheet").style.display="block";
         document.getElementById("yourResPaper").style.display="block";
         document.getElementById("compResPaper").style.display="block";
+        tie.style.display="block";
 
         document.getElementById("resYourScoreVAR").innerHTML = localStorage.getItem('SPS_User_Score');
         document.getElementById("resOppScoreVAR").innerHTML = localStorage.getItem('SPS_Opp_Score');
@@ -93,7 +130,10 @@ paperButton.addEventListener("click", () => {
         document.getElementById("mainSheet").style.display="none";
         document.getElementById("resultSheet").style.display="block";
         document.getElementById("yourResPaper").style.display="block";
+        document.getElementById("yourResPaper").style.animation = "lose-breathe 4s ease-in-out infinite"
         document.getElementById("compResScissor").style.display="block";
+        document.getElementById("compResScissor").style.animation = "win-breathe 4s ease-in-out infinite"
+        loser.style.display="block";
         
         localStorage.setItem('SPS_Opp_Score', parseInt(localStorage.getItem('SPS_Opp_Score'))+1);
         document.getElementById("resYourScoreVAR").innerHTML = localStorage.getItem('SPS_User_Score');
@@ -109,7 +149,10 @@ scissorButton.addEventListener("click", () => {
         document.getElementById("mainSheet").style.display="none";
         document.getElementById("resultSheet").style.display="block";
         document.getElementById("yourResScissor").style.display="block";
+        document.getElementById("yourResScissor").style.animation = "lose-breathe 4s ease-in-out infinite"
         document.getElementById("compResStone").style.display="block";
+        document.getElementById("compResStone").style.animation = "win-breathe 4s ease-in-out infinite"
+        loser.style.display="block";
         
         localStorage.setItem('SPS_Opp_Score', parseInt(localStorage.getItem('SPS_Opp_Score'))+1);
         document.getElementById("resYourScoreVAR").innerHTML = localStorage.getItem('SPS_User_Score');
@@ -119,7 +162,11 @@ scissorButton.addEventListener("click", () => {
         document.getElementById("mainSheet").style.display="none";
         document.getElementById("resultSheet").style.display="block";
         document.getElementById("yourResScissor").style.display="block";
+        document.getElementById("yourResScissor").style.animation = "win-breathe 4s ease-in-out infinite"
         document.getElementById("compResPaper").style.display="block";
+        document.getElementById("compResPaper").style.animation = "lose-breathe 4s ease-in-out infinite"
+        nextButton.style.display="block";
+        winner.style.display="block";
 
         localStorage.setItem('SPS_User_Score', parseInt(localStorage.getItem('SPS_User_Score'))+1);
         document.getElementById("resYourScoreVAR").innerHTML = localStorage.getItem('SPS_User_Score');
@@ -130,6 +177,7 @@ scissorButton.addEventListener("click", () => {
         document.getElementById("resultSheet").style.display="block";
         document.getElementById("yourResScissor").style.display="block";
         document.getElementById("compResScissor").style.display="block";
+        tie.style.display="block";
 
         document.getElementById("resYourScoreVAR").innerHTML = localStorage.getItem('SPS_User_Score');
         document.getElementById("resOppScoreVAR").innerHTML = localStorage.getItem('SPS_Opp_Score');
